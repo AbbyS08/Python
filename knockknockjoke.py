5a749dc887ea3f3d0b9917365a9f0e2f3484bee5
AbbyS08 <astandish@students.mchenry.edu>
2025-05-28 22:28:11 -0500

    Adding knock knock joke

---
+++ knockknockjoke.py
@@ -0,0 +1,4 @@
+# Knock Knock Joke
+# knock knock joke source: https://www.goodhousekeeping.com/life/parenting/g36198919/knock-knock-jokes-for-kids/
+print("Knock knock.", "\nWho's there?", "\nRadio.",
+      "\nRadio who?", "\nRadio not, here I come!")
