d05e93735bf195630ddda5c99e0ee148a028a005
AbbyS08 <astandish@students.mchenry.edu>
2025-05-31 16:39:24 -0500

    Adding Comments

---
+++ comments.py
@@ -0,0 +1,12 @@
+# tank size is the amount of gallons a tank can hold
+tank_size = 15
+# Define the total distance covered
+# distance is in miles
+distance = 600
+# Calculate miles per gallon
+# mpg: abbreviation for miles per gallon
+mpg = distance / tank_size
+# Print the result
+print("Miles per gallon:", mpg)
+# the code calculates the miles per gallon of a vehicle based on the amount of distance
+# covered (miles) with one tank of fuel (gallons)
