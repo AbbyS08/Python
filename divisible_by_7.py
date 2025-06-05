265303e34b0d18f3721dd47b2172d70e983928b5
AbbyS08 <astandish@students.mchenry.edu>
2025-06-05 10:28:54 -0500

    Using for loops

---
+++ divisible_by_7.py
@@ -0,0 +1,4 @@
+# Program that prints the numbers divisible by 7 between 1 and 300
+for i in range(1, 301):  # Range of 1-300
+    if i % 7 == 0:  # Checking if divisible by 7
+        print(i)  # Printing if divisible by 7
