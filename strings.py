269e8bb6504710278b09f31b9a4831c77d105c95
AbbyS08 <astandish@students.mchenry.edu>
2025-07-06 08:27:15 -0500

    Exploring String Functions

---
+++ strings.py
@@ -0,0 +1,28 @@
+def main():
+
+    # Example string
+    example_string = "Hello, Python programmers!"
+    # TODO: Iterate through the string and print each character
+    print("Iterating through the string:")
+    for i in example_string:
+        print(i, end=" ")
+    # TODO: Find and print the character with the minimum ASCII value in the string
+    print("\nCharacter with the minimum ASCII value:")
+    minimum = min(example_string)
+    print(minimum)
+    # TODO: Find and print the character with the maximum ASCII value in the string
+    print("\nCharacter with the maximum ASCII value:")
+    maximum = max(example_string)
+    print(maximum)
+    # TODO: Find and print the index of the first occurrence of 'o' in the string
+    print("\nIndex of 'o':")
+    print(example_string.index("o"))
+    # TODO: Convert the string into a list of characters and print the list
+    print("\nConverting string to a list of characters:")
+    print(list(example_string))
+    # TODO: Count and print the number of occurrences of 'o' in the string
+    print("\nCount of 'o' in the string:")
+    print(example_string.count("o"))
+
+
+main()
