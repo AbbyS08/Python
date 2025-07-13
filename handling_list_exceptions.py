78f5148a48963a11a96a258a81e05edcfb3abfcc
AbbyS08 <astandish@students.mchenry.edu>
2025-07-13 16:42:44 -0500

    handling_list_exceptions.py

--- handling_list_exceptions.py
+++ handling_list_exceptions.py
@@ -3,11 +3,14 @@
 def main():
     top_artists = ["The Beatles", "Madonna", "Elton John", "Elvis Presley", "Mariah Carey", "Stevie Wonder", "Janet Jackson", "Michael Jackson", "Whitney Houston", "Rihanna"]
     try:
+        # Asking for an index and artist name
         index = int(input("Enter the index of the artist to replace: "))
         new_name = input("Enter the new artist name: ")
+        # Replacing the artist at the index
         top_artists[index] = new_name
+        # Priting the new list
         print(f"Updated list: {top_artists}")
-    
+    # Error messages
     except (ValueError, IndexError):
         print("An input error occurred.")
     except:
