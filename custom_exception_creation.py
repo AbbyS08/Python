a18bbb3b645275e9df12bb56dd00fcf533591e70
AbbyS08 <astandish@students.mchenry.edu>
2025-07-26 19:30:08 -0500

    Update custom_exception_creation.py

--- custom_exception_creation.py
+++ custom_exception_creation.py
@@ -6,23 +6,22 @@ class NotNumericError(Exception):
         self.message = message
 
 def main():
-    try:
-        # Entering the loop
-        number_input = "a"
-        while number_input.isnumeric() == False:
+    input_is_valid = False # Flag
+    while not input_is_valid:
+        try:
             # Asking for a number
             number_input = input("Enter a number: ")
             # Raising the custom exception if the input isn't a number
             if not number_input.isnumeric():
                 raise NotNumericError("The input is not a number.")
-    # Error message
-    except NotNumericError as e:
-        print(f"Caught an error: {e.message}")
-        main() # Rerunning the program because the loop doesn't work with exceptions
-    # Printing confirmation message if the input is valid
-    else:
-        print(f"Input is valid: {number_input}")
-    finally:
-        print("End of program execution.")
+        # Error message
+        except NotNumericError as e:
+            print(f"Caught an error: {e.message}")
+        # Printing confirmation message if the input is valid
+        else:
+            print(f"Input is valid: {number_input}")
+            input_is_valid = True # Changing the flag if the loop should be exited
+        finally:
+            print("End of program execution.")
 
 main()
