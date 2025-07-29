1d885e52d738261be84cdda11cb966492d918190
AbbyS08 <astandish@students.mchenry.edu>
2025-07-29 12:21:40 -0500

    Update personal_diary.py

--- diary/personal_diary.py
+++ diary/personal_diary.py
@@ -1,16 +1,23 @@
 # Personal Diary
+import os
 
 def main():
 
     # Asking the user for a date, time, and diary entry
-    date = input("Enter date: ")
-    time = input("Enter time: ")
-    entry = input("Enter diary entry: ")
-
-    # Creating the file in append mode
-    with open('8/diary/diary.txt', 'a') as file:  
-        # Writing the three things inputted to the diary.txt file
-        file.write(date + ', ' + time + '\n'+ entry +'\n')
-    file.close()
+    try:
+        date = input("Enter date: ")
+        time = input("Enter time: ")
+        entry = input("Enter diary entry: ")
+        #ensure that the directory exists already
+        os.makedirs("8/diary",exist_ok=True)
+        # Creating the file in append mode
+        with open('8/diary/diary.txt', 'a') as file:  
+            # Writing the three things inputted to the diary.txt file
+            file.write(date + ', ' + time + '\n'+ entry +'\n')
+        file.close()
+    except FileNotFoundError:
+        print("File not found.")
+    except:
+        print("Something went wrong.")
 
 main()
